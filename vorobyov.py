a4b44d926e9895e421a1fa1ba62a0776bf31bbe3
Vitaliy Vorobyov <vitaliy_vorobyov@Vitaliys-MacBook-Pro.local>
2019-02-28 19:56:00 +0200

    Added files

--- vorobyov.py
+++ vorobyov.py
@@ -1,7 +1,16 @@
 def read_file(file_name):
+
+    lst = []
+
     with open(file_name, "r") as f:
-        return f.read()
+        number = int(f.readline())
+
+        for i in range(number):
+            a = f.readline().strip().split()
+            lst.append([a[0], a[1], a[2:]])
+
+    return lst
 
 
 if __name__ == '__main__':
-    pass
+    print(read_file("in/a_example.txt"))
