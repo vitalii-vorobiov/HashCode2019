d691198bdd9ebf4ff9997641cdff93c703349e11
Markiian <matsiuk@ucu.edu.ua>
2019-02-28 19:56:06 +0200

    read

--- matsiuk.py
+++ matsiuk.py
@@ -1,4 +1,12 @@
 def file_in(path):
+    res = []
     with open(path, "r") as f:
-        return f.read()
+        print(f"n= {f.readline()}")
+        for line in f:
+            res_v = line.split()
+            res.append((res_v[0], set(res_v[2:])))
+    print(len(res))
+    return res
 
+x = file_in("in/a_example.txt")
+# print(x)
