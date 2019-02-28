56172e92f12e5cd87a7a592d7d0d34bae81d203f
Markiian <matsiuk@ucu.edu.ua>
2019-02-28 20:21:12 +0200

    merge

--- matsiuk.py
+++ matsiuk.py
@@ -1,12 +1,30 @@
 def file_in(path):
-    res = []
+    res_h = []
+    res_v = []
     with open(path, "r") as f:
         print(f"n= {f.readline()}")
+        counter = 0
         for line in f:
-            res_v = line.split()
-            res.append((res_v[0], set(res_v[2:])))
-    print(len(res))
+            res_word = line.split()
+            if res_word[0] == 'H':
+                res_h.append((counter, set(res_word[2:])))
+            else:
+                res_v.append((counter, set(res_word[2:])))
+            counter+=1
+    # print(len(res))
+    return res_h, res_v
+
+
+def item_merge(it1, it2):
+    return f"{it1[0]} {it2[0]}", it1[1].union(it2[1])
+
+def merge(lst):
+    res=[]
+    for i in range(0, len(lst),2):
+        res.append(item_merge(lst[i],lst[i+1]))
     return res
 
-x = file_in("in/a_example.txt")
-# print(x)
+
+x,y = file_in("in/a_example.txt")
+print("h",x)
+print("v", merge(y))
